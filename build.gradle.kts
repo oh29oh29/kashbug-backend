4aa57203af0581b0d1205a0284b68274ebd468cf
oh29oh29 <hyukjae89@gmail.com>
2020-11-27 22:36:01 +0900

    초기 세팅 - DB 및 JPA 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,11 +17,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
---
+++ src/main/kotlin/com/kashbug/kashbugbackend/domain/user/User.kt
@@ -0,0 +1,10 @@
+package com.kashbug.kashbugbackend.domain.user
+
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+class User {
+    @Id
+    val id: String = ""
+}
