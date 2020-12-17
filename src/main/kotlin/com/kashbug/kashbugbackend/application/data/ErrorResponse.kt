9ecb975bf39262562550546bb9c61ce6e83c697e
oh29oh29 <hyukjae89@gmail.com>
2020-12-17 23:04:56 +0900

    [build-foundation] 기본 응답 객체 및 ExceptionHandler 추가

---
+++ src/main/kotlin/com/kashbug/kashbugbackend/application/data/ErrorResponse.kt
@@ -0,0 +1,9 @@
+package com.kashbug.kashbugbackend.application.data
+
+data class ErrorResponse(
+
+    val code: String,
+    val name: String,
+    val message: String
+
+)
---
+++ src/main/kotlin/com/kashbug/kashbugbackend/application/data/OkResponse.kt
@@ -0,0 +1,10 @@
+package com.kashbug.kashbugbackend.application.data
+
+data class OkResponse<T> (
+
+    val code: String = ResponseCode.STATUS_SUCCESS.code,
+    val name: String = ResponseCode.STATUS_SUCCESS.name,
+    val message: String = ResponseCode.STATUS_SUCCESS.message,
+    val data: T? = null
+
+)
