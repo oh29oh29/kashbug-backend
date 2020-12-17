b80ef982a3f47a99a3fd4e4f3548c2b258257bdb
oh29oh29 <hyukjae89@gmail.com>
2020-12-17 23:12:33 +0900

    [security-config] WebSecurity config 추가

--- src/main/kotlin/com/kashbug/kashbugbackend/config/SecurityConfig.kt
+++ src/main/kotlin/com/kashbug/kashbugbackend/config/SecurityConfig.kt
@@ -1,6 +1,8 @@
 package com.kashbug.kashbugbackend.config
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -8,10 +10,23 @@ import org.springframework.security.config.http.SessionCreationPolicy
 @EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
+    override fun configure(web: WebSecurity) {
+        // static 자원에 대한 Security 설정을 적용하지 않음
+        web
+                .ignoring()
+                .requestMatchers(
+                        PathRequest
+                                .toStaticResources()
+                                .atCommonLocations()
+                )
+
+    }
+
     override fun configure(http: HttpSecurity) {
         http
                 .httpBasic().disable()
                 .csrf().disable()
+                .formLogin().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증 (세션 사용 X)
                 .and()
                 .authorizeRequests()
